7ef5a3e949be941b06e86c6e45f6c905719549a2
Shahed <shaheduz.zaman@northsouth.edu>
2020-05-23 11:47:19 +0600

    initial commit: files and works up to adding collaider to bg, bird, ground added

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float speed;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            //Jump
+            rb.velocity = new Vector2(rb.velocity.x, speed);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, speed);
+        }
+	}
+}
