1c099935d513d7da3c4e980de6522a9ceb8c8d84
Shahed <shaheduz.zaman@northsouth.edu>
2020-07-07 23:09:41 +0600

    score panel: score and high score functional, bug fixed!

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Score : MonoBehaviour {
+
+    int highScore;
     Text scoreText;
     int score = 0;
+
+    public Text panelScore;
+    public Text panelHighScore;
+
     // Use this for initialization
     void Start () {
-        scoreText = GetComponent<Text>();	
+        scoreText = GetComponent<Text>();
+        //panelScore = GetComponent<Text>();
+        highScore = PlayerPrefs.GetInt("HighScore");
+        panelHighScore.text = highScore.ToString();
 	}
 	
 	// Update is called once per frame
@@ -19,5 +28,13 @@ public class Score : MonoBehaviour {
     {
         score++;
         scoreText.text = score.ToString();
+        panelScore.text = score.ToString();
+
+        if (score > highScore)
+        {
+            highScore = score;
+            panelHighScore.text = highScore.ToString();
+            PlayerPrefs.SetInt("HighScore", highScore);
+        }
     }
 }
